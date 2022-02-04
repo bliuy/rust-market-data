049cbe089b4833b6e0aa6e87f6eaf8060e8164f2
bliu01510 <liuy01510.work@gmail.com>
2022-02-04 10:40:10 +0000

    Implemented the logic for reading the csv file returned from the HTTP request.

--- src/lib.rs
+++ src/lib.rs
@@ -1,65 +1,120 @@
+use serde::{Deserialize, Serialize};
 use std::string::ParseError;
 
 use chrono::TimeZone;
 
+#[derive(Debug, Serialize, Deserialize)]
 pub enum Currency {
-    USD(f64),
-    SGD(f64),
+    USD,
+    SGD,
 }
 
-pub enum Data_Provider {
-    Yahoo_Finance,
+pub enum DataProvider {
+    YahooFinance,
 }
 
-pub struct HistoricalPrices {
+mod csv_datetime_formatting {
+    use chrono::TimeZone;
+    use serde::Deserialize;
+
+    // use super::*;
+    static SERIALIZE_DATETIME_FORMAT_STRING: &str = "%Y-%m-%d";
+    static DESERIALIZE_DATETIME_FORMAT_STRING: &str = "%Y-%m-%d %H:%M:%S";
+
+    pub fn serialize<S>(
+        date: &chrono::DateTime<chrono::Utc>,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let s = format!("{}", date.format(SERIALIZE_DATETIME_FORMAT_STRING));
+        serializer.serialize_str(&s)
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<chrono::DateTime<chrono::Utc>, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let mut s = String::deserialize(deserializer)?;
+        s.push_str(" 00:00:00");
+        let result = chrono::Utc
+            .datetime_from_str(&s, DESERIALIZE_DATETIME_FORMAT_STRING)
+            .map_err(serde::de::Error::custom);
+        result
+    }
+}
+
+pub struct PriceRecord {
     ticker_symbol: String,
-    timestamp: Vec<chrono::DateTime<chrono::Utc>>,
-    open_price: Vec<Currency>,
-    high_price: Vec<Currency>,
-    low_price: Vec<Currency>,
-    close_price: Vec<Currency>,
+    timestamp: chrono::DateTime<chrono::Utc>,
+    open_price: Currency,
+    high_price: Currency,
+    low_price: Currency,
+    close_price: Currency,
+    adj_close: Currency,
+    volume: i32,
 }
 
-impl HistoricalPrices {
-    fn _load_from_datasource(
-        ticker_symbol: &str,
-        start_datetime: chrono::DateTime<chrono::Utc>,
-        end_datetime: chrono::DateTime<chrono::Utc>,
-    ) {
-        let base_url = "https://query1.finance.yahoo.com/v7/finance/download/XLK?";
-        let period1 = start_datetime.timestamp();
-        let period2 = end_datetime.timestamp();
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CsvRecord {
+    #[serde(rename = "Date")]
+    #[serde(with = "csv_datetime_formatting")]
+    timestamp: chrono::DateTime<chrono::Utc>,
+    #[serde(rename = "Open")]
+    open_price: f64,
+    #[serde(rename = "High")]
+    high_price: f64,
+    #[serde(rename = "Low")]
+    low_price: f64,
+    #[serde(rename = "Close")]
+    close_price: f64,
+    #[serde(rename = "Adj Close")]
+    adj_close: f64,
+    #[serde(rename = "Volume")]
+    volume: i64,
+}
 
-        // Constructing the complete url string to make the request
-        let url = format!(
-            "{}?period1={}&period2={}&interval=1d&events=history&includeAdjustedClose=true",
-            base_url, period1, period2
-        );
+pub fn get_prices(
+    ticker_symbol: &str,
+    start_datetime: chrono::DateTime<chrono::Utc>,
+    end_datetime: chrono::DateTime<chrono::Utc>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let base_url = "https://query1.finance.yahoo.com/v7/finance/download/XLK?";
+    let period1 = start_datetime.timestamp();
+    let period2 = end_datetime.timestamp();
+    let mut csv_vec: Vec<CsvRecord>=Vec::new();
 
-        // Sending the GET request
-        let response_bytes = reqwest::blocking::get(url)
-            .unwrap()
-            .bytes()
-            .unwrap()
-            .into_iter()
-            .collect::<Vec<u8>>();
+    // Constructing the complete url string to make the request
+    let url = format!(
+        "{}?period1={}&period2={}&interval=1d&events=history&includeAdjustedClose=true",
+        base_url, period1, period2
+    );
 
-        // Parsing the response into a csv file
-        let rdr = csv::ReaderBuilder::new().from_reader(&response_bytes[..]);
+    // Sending the GET request
+    let response_bytes = reqwest::blocking::get(url)
+        .expect("GET request failed!")
+        .bytes()
+        .unwrap()
+        .into_iter()
+        .collect::<Vec<u8>>(); // Collecting into a byte array
+
+    // Parsing the response into a csv file
+    let mut rdr = csv::ReaderBuilder::new().from_reader(&response_bytes[..]);
+
+    // Deserializing the csv file
+    for result in rdr.deserialize() {
+        let csv_record: CsvRecord = result?;
+        csv_vec.push(csv_record);
     }
 
-    fn new(
-        ticker_symbol: String,
-        start_datetime: chrono::DateTime<chrono::Utc>,
-        end_datetime: chrono::DateTime<chrono::Utc>,
-    ) {
-    }
+    Ok(())
 }
 
 pub fn chrono_strptime(
     timestamp: &str,
 ) -> std::result::Result<chrono::DateTime<chrono::Utc>, Box<dyn std::error::Error>> {
-    /// Function takes a string datetime formatted as "YYYY-MM-DD" and converts it to a chrono::Utc object.
+    // Function takes a string datetime formatted as "YYYY-MM-DD" and converts it to a chrono::Utc object.
     let datetime_array_result: std::result::Result<Vec<_>, _> =
         timestamp.split("-").map(|x| x.parse::<u32>()).collect();
 
@@ -92,12 +147,16 @@ mod tests {
     }
 
     #[test]
-    fn test_load_from_datasource() {
-        HistoricalPrices::_load_from_datasource(
+    fn test_get_prices() {
+        match get_prices(
             "XLK",
             chrono::Utc.ymd(2022, 1, 1).and_hms(0, 0, 0),
             chrono::Utc.ymd(2022, 2, 1).and_hms(0, 0, 0),
-        )
+        ) {
+            Ok(i) => println!("OK"),
+            Err(e) => println!("{}",e)
+        };
+        ()
     }
 
     // #[test]
