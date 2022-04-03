916dc19a13d36b948dc846953dfaa9645a366cb6
bliu01510 <liuy01510.work@gmail.com>
2022-04-03 03:53:48 +0000

    Removal of underscores from vars.

--- src/requests.rs
+++ src/requests.rs
@@ -1,13 +1,13 @@
 //! This module contains all the functions required to perform a HTTP request
 use super::errors;
-use reqwest;
+
 
 
 pub fn blocking_reqwest(url: &str) -> Result<reqwest::blocking::Response, errors::SourceDataError> {
     let _response = reqwest::blocking::get(url);
     let response = match _response {
         Ok(resp) => Ok(resp),
-        Err(err) => Err(errors::SourceDataError::ConnectionError(format!(
+        Err(_err) => Err(errors::SourceDataError::ConnectionError(format!(
             "Error when attempting to connect to the following url: \n {}",
             url
         ))),
