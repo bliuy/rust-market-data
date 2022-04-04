144e38528e090f9a8963bdc40cb969adf6e6c09b
bliu01510 <liuy01510.work@gmail.com>
2022-04-04 00:41:13 +0000

    Added InitializationError enum.

--- src/errors.rs
+++ src/errors.rs
@@ -1,7 +1,5 @@
 //! Defining custom errors for the Market_Analysis crate.
 
-
-
 #[derive(Debug)]
 pub enum SourceDataError {
     ConnectionError(String),
@@ -14,11 +12,31 @@ impl std::fmt::Display for SourceDataError {
     fn fmt(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
         // Performing pattern matching
         match self {
-            SourceDataError::ConnectionError(msg) => {
-                std::fmt::write(formatter, format_args!("Error raised during the retrieval of source data: {}", msg))
-            }
-            SourceDataError::ParseError(msg) => {
-                std::fmt::write(formatter, format_args!("Error raised during the parsing of source data: {}", msg))
+            SourceDataError::ConnectionError(msg) => std::fmt::write(
+                formatter,
+                format_args!("Error raised during the retrieval of source data: {}", msg),
+            ),
+            SourceDataError::ParseError(msg) => std::fmt::write(
+                formatter,
+                format_args!("Error raised during the parsing of source data: {}", msg),
+            ),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum InitializationError {
+    TickerInfoInitializationError(String),
+}
+
+impl std::error::Error for InitializationError {}
+
+impl std::fmt::Display for InitializationError {
+    fn fmt(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        // Performing pattern matching
+        match self {
+            InitializationError::TickerInfoInitializationError(err) => {
+                std::fmt::write(formatter, format_args!("Error occured during the initialization of the TickerInfo struct object. See the error raised: {}", err))
             }
         }
     }
@@ -30,14 +48,13 @@ mod tests {
 
     #[test]
     fn SourceDataError() {
-
         let test_func = || SourceDataError::ConnectionError("Test Connection Error.".to_string());
         let returned_error = test_func();
-        match returned_error{
-            SourceDataError::ConnectionError(i) => {assert!(i==*"Test Connection Error.")},
-            _ => panic!("Assertion failed.")
+        match returned_error {
+            SourceDataError::ConnectionError(i) => {
+                assert!(i == *"Test Connection Error.")
+            }
+            _ => panic!("Assertion failed."),
         }
-
     }
-
 }
