6cac5e17d48d1894d4dbebb7aeca3dd449914a41
bliu01510 <liuy01510.work@gmail.com>
2022-04-04 00:41:48 +0000

    Added constructor for TickerInfo + Created Unit Tests.

--- src/datasets.rs
+++ src/datasets.rs
@@ -3,12 +3,50 @@ use super::enums;
 use super::errors;
 use super::parsers;
 use super::requests;
+use chrono;
+use chrono::TimeZone;
 
-
+#[derive(PartialEq)]
 struct TickerInfo<'a> {
     ticker_symbol: &'a str,
-    start_date: chrono::DateTime<chrono::Utc>,
-    end_date: chrono::DateTime<chrono::Utc>,
+    start_datetime: chrono::DateTime<chrono::Utc>,
+    end_datetime: chrono::DateTime<chrono::Utc>,
+}
+
+impl<'a> TickerInfo<'a> {
+    fn new(
+        ticker_symbol: &'a str, // Guarantees that the reference to the char array (&str) lives as long as the TickerInfo object.
+        start_timestamp: &str,
+        end_timestamp: &str,
+    ) -> Result<Self, errors::InitializationError> {
+
+        const DATE_FORMAT: &str = "%Y-%m-%d  %H:%M:%S";
+
+        // Processing the start and end dates into chrono datetime objects
+        let start_datetime = match chrono::Utc.datetime_from_str(start_timestamp, DATE_FORMAT) {
+            Ok(i) => i,
+            Err(e) => {
+                return Err(errors::InitializationError::TickerInfoInitializationError(
+                    String::from(format!("{:#?}", e)), // Returning the printed output of the internal error.
+                ))
+            }
+        };
+        let end_datetime = match chrono::Utc.datetime_from_str(end_timestamp, DATE_FORMAT) {
+            Ok(i) => i,
+            Err(e) => {
+                return Err(errors::InitializationError::TickerInfoInitializationError(
+                    String::from(format!("{:#?}", e)), // Returning the printed output of the internal error.
+                ))
+            }
+        };
+
+        // Constructing the TickerInfo Object
+        Ok(TickerInfo{
+            ticker_symbol,
+            start_datetime,
+            end_datetime, 
+        })
+    }
 }
 
 struct RawPrice<'a> {
@@ -26,8 +64,8 @@ fn source_yahoo_finance<'a>(
     ticker_info: &'a TickerInfo,
 ) -> Result<Vec<RawPrice<'a>>, errors::SourceDataError> {
     const BASE_URL: &str = "https://query1.finance.yahoo.com/v7/finance/download";
-    let start_date = ticker_info.start_date;
-    let end_date = ticker_info.end_date;
+    let start_date = ticker_info.start_datetime.timestamp();
+    let end_date = ticker_info.end_datetime.timestamp();
     let ticker = ticker_info.ticker_symbol;
 
     // Defining the record struct for parsing the response content
@@ -83,5 +121,31 @@ fn source_yahoo_finance<'a>(
         })
         .collect::<Vec<Record>>();
 
+    println!("{:#?}", records);
+
+
     todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ticker_info_struct_new() {
+        let foo = TickerInfo::new("AAPL", "2020-01-01 00:00:00", "2020-01-03 00:00:00").unwrap(); // Unwrapped used, panics will cause the test to fail.
+        let bar = TickerInfo{
+            ticker_symbol: "AAPL",
+            start_datetime: chrono::Utc.ymd(2020, 1, 1).and_hms(0,0,0),
+            end_datetime: chrono::Utc.ymd(2020, 1, 3).and_hms(0,0,0),
+        };
+        assert!(foo==bar)
+    }
+
+    #[test]
+    fn test_source_yahoo_finance() {
+        let foo = TickerInfo::new("AAPL", "2020-01-01 00:00:00", "2020-01-07 00:00:00").unwrap(); // Unwrapped used, panics will cause the test to fail.
+        source_yahoo_finance(&foo);
+    }
+    
 }
