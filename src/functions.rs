832551e52656dd5563a4a4899361e9c138c62e44
bliu01510 <liuy01510.work@gmail.com>
2022-04-16 01:57:31 +0000

    Splitting off the grouping function into an internal grouping function to allow for type casting of the fn pointer.

--- src/functions.rs
+++ src/functions.rs
@@ -4,6 +4,7 @@ use super::datasets;
 use super::errors::AggregationError;
 use chrono;
 use itertools::Itertools;
+use num_traits::{self, Num};
 
 mod grouping {
     use super::*;
@@ -16,12 +17,16 @@ mod grouping {
         values: &'a [U],
     ) -> Result<
         itertools::GroupBy<
-            u32,
+            chrono::DateTime<chrono::Utc>,
             Zip<Iter<'a, T>, Iter<'a, U>>,
-            fn(&(&T, &U)) -> u32,
+            fn(&(&T, &U)) -> chrono::DateTime<chrono::Utc>,
         >,
         AggregationError,
-    > where T: chrono::Datelike, U: PartialOrd {
+    >
+    where
+        T: chrono::Datelike,
+        U: Ord + Copy + num_traits::Num,
+    {
         // Validating that the lengths of the arrays are equal.
         if timestamps.len() != values.len() {
             return Err(AggregationError::InconsistentLengthError(format!(
@@ -32,14 +37,27 @@ mod grouping {
         }
 
         // Defining the grouping function
-        fn _grouping_function<X,Y>(x: &(&X, &Y)) -> u32 where X: chrono::Datelike, Y: PartialOrd {
+        fn _grouping_function<X, Y>(x: &(&X, &Y)) -> chrono::DateTime<chrono::Utc>
+        where
+            X: chrono::Datelike,
+            Y: PartialOrd,
+        {
             let (timestamp, value) = x;
-            timestamp.iso_week().week0()
-        } // NOTE: A closure will not work, since the GroupBy struct field will require an actual specific type, while impl Traits are only viable for function signatures.
+            let current_week = timestamp.iso_week().week0();
+            let week: chrono::DateTime<chrono::Utc> = chrono::Utc::today()
+                .and_hms(0, 0, 0)
+                .with_ordinal0(current_week * 7)
+                .unwrap();
+            week
+        } // NOTE: A closure (anonymous type) will not work as it cannot be defined within the , since the GroupBy struct field will require an actual specific type, while impl Traits are only viable for function signatures.
+          // Following error will be returned if closure is used:
+          // mismatched types
+          // expected struct `itertools::GroupBy<_, std::iter::Zip<std::slice::Iter<'a, _>, std::slice::Iter<'a, _>>, for<'r, 's, 't0> fn(&'r (&'s T, &'t0 U)) -> u32>`
+          //    found struct `itertools::GroupBy<_, std::iter::Zip<std::slice::Iter<'_, _>, std::slice::Iter<'_, _>>, [closure@src/functions.rs:49:61: 50:42]>`
 
         // Performing casting of the function - Coercing from Function Item (idenfication via distinct type) to Function Pointer (identification via stored address)
         let grouping_function =
-            _grouping_function as fn(&(&T, &U)) -> u32;
+            _grouping_function as fn(&(&T, &U)) -> chrono::DateTime<chrono::Utc>;
 
         // The above step helps to avoid the following error:
         // expected struct `itertools::GroupBy<_, std::iter::Zip<std::slice::Iter<'a, _>, std::slice::Iter<'a, _>>, for<'r, 's, 't0> fn(&'r (&'s DateTime<Utc>, &'t0 f32)) -> _>`
@@ -49,5 +67,49 @@ mod grouping {
         let result = timestamps.iter().zip(values).group_by(grouping_function);
 
         Ok(result)
+    }
+}
+
+mod AggregationFunctions {
+
+    use super::*;
+
+    // Defining custom types
+    type AggregationResult<T: chrono::Datelike, U: Ord> = std::collections::HashMap<T, U>;
+
+    fn max<'a, T, U>(
+        groupby: itertools::GroupBy<
+            chrono::DateTime<chrono::Utc>,
+            std::iter::Zip<std::slice::Iter<'a, T>, std::slice::Iter<'a, U>>,
+            fn(&(&T, &U)) -> chrono::DateTime<chrono::Utc>,
+        >,
+    ) -> AggregationResult<chrono::DateTime<chrono::Utc>, U>
+    where
+        T: chrono::Datelike,
+        U: Ord + Copy + num_traits::Num,
+    {
+        // Creating new AggregationResult object
+        let mut result: AggregationResult<chrono::DateTime<chrono::Utc>, U> =
+            AggregationResult::new();
+
+        // Processing of the individual groups
+        let result: AggregationResult<chrono::DateTime<chrono::Utc>, U> = groupby
+            .into_iter()
+            .map(|(k, v)| {
+                let max_value = match v.map(|(x, &y)| y).max() {
+                    Some(i) => i,
+                    None => {
+                        let zero = match <U as Num>::from_str_radix("0", 10) {
+                            Ok(j) => j,
+                            Err(e) => unreachable!(),
+                        };
+                        zero
+                    }
+                };
+                (k, max_value)
+            })
+            .collect();
+
+        result
     }
 }
